5f90571dcb6b55192962b3c7f992c264b690615e
Mylene <pont.mylene@gmail.com>
2022-01-11 16:33:15 +0900

    template string for link

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ const ulEl = document.getElementById("ul-el")
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     // Clear out the input field
+    inputEl.value = ""
     renderLeads()
 })
 
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
     }
     ulEl.innerHTML = listItems  
 }
