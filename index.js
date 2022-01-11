0b2cde414b185d3c24559e44f470142dae0f9f4a
Mylene <pont.mylene@gmail.com>
2022-01-11 17:23:14 +0900

    local storage added, stop before truthy/ falsy

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     // Clear out the input field
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
 })
 
