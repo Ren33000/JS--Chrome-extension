0d79ab294eca8a91cb28ebd358d4d09ed2d06a2d
Mylene <pont.mylene@gmail.com>
2022-01-12 19:41:55 +0900

    save tabs working

--- index.js
+++ index.js
@@ -11,14 +11,12 @@ if (leadsFromLocalStorage) {
     render(myLeads)
 }
 
-const tabs = [
-    {url: "https://pomodoro-tracker.com/"}
-]
-
-tabBtn.addEventListener("click", function(){
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
 })
 
 
